925d4f9a7d9134e992fd2acf82faf79b32d25c97
Gal Dolber <gal.dolber@gmail.com>
2021-01-13 13:49:33 -0300

    Ignores + HtmlWebpackPlugin + webpack.optimize.LimitChunkCountPlugin=2

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const dist = path.resolve(__dirname, "dist");
+
 module.exports = {
     entry: './src/bootstrap.ts',
-    resolve: {
-        fallback: {
-          util: require.resolve("util/")
-        }
-    },
-    experiments: {
-        syncWebAssembly: true,
-      },
-  };
+    plugins: [
+	new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 2
+	}),
+	new HtmlWebpackPlugin({
+            hash: true,
+            filename: 'index.html'
+        })
+    ],
+    module: {
+	rules: [
+	    {
+		test: /\.css$/,
+		use: [
+		    'style-loader',
+		    'css-loader'
+		]
+	    },
+	    {
+		test: /\.(png|svg|jpg|gif)$/,
+		use: [
+		    'file-loader'
+		]
+	    }
+	]
+    }
+};

6ef7f6c10bcdba919b4bbc3085cfa82352f2d953
Gal Dolber <gal.dolber@gmail.com>
2021-01-13 14:23:05 -0300

    Before cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,3 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-const dist = path.resolve(__dirname, "dist");
-
 module.exports = {
-    entry: './src/bootstrap.ts',
-    plugins: [
-	new webpack.optimize.LimitChunkCountPlugin({
-            maxChunks: 2
-	}),
-	new HtmlWebpackPlugin({
-            hash: true,
-            filename: 'index.html'
-        })
-    ],
-    module: {
-	rules: [
-	    {
-		test: /\.css$/,
-		use: [
-		    'style-loader',
-		    'css-loader'
-		]
-	    },
-	    {
-		test: /\.(png|svg|jpg|gif)$/,
-		use: [
-		    'file-loader'
-		]
-	    }
-	]
-    }
+    entry: './src/bootstrap.ts'
 };
